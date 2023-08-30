d72950a6397b5f3a3736386391b83ce1c961d938
Kevin Borup - Station <kevi1291@zbc.dk>
2023-08-30 21:58:03 +0200

    test

--- Scrumboard/ScrumboardApp/lib/screens/scrum_board_screen.dart
+++ Scrumboard/ScrumboardApp/lib/screens/scrum_board_screen.dart
@@ -12,24 +12,7 @@ class ScrumBoardScreen extends StatefulWidget {
 }
 
 class _ScrumBoardScreenState extends State<ScrumBoardScreen> {
-  final AppFlowyBoardController boardData = AppFlowyBoardController();
   late List<BoardColumnModel> columns;
-
-  @override
-  void initState() {
-    for (var column in columns) {
-      final newColumn = AppFlowyGroupData(
-          id: column.id,
-          name: column.title,
-          items: column.notes
-              .map((BoardNoteModel note) => TextItem(note.richedText))
-              .toList());
-
-      boardData.addGroup(newColumn);
-    }
-
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,27 +20,9 @@ class _ScrumBoardScreenState extends State<ScrumBoardScreen> {
       appBar: AppBar(
         title: const Text("Test"),
       ),
-      body: Expanded(
-        child: AppFlowyBoard(
-          controller: boardData,
-          cardBuilder: (context, column, columnItem) {
-            final textItem = columnItem as TextItem;
-            return AppFlowyGroupCard(
-              key: const ObjectKey(TextItem),
-              child: Padding(padding: const EdgeInsets.fromLTRB(7, 2, 7, 2),  child: RichText(text: TextSpan(text: textItem.s))),
-            );
-          },
-        ),
+      body: const Expanded(
+        child: Placeholder()
       ),
     );
   }
-}
-
-class TextItem extends AppFlowyGroupItem {
-  final String s;
-
-  TextItem(this.s);
-
-  @override
-  String get id => s;
 }
